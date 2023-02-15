c44bf5eb3d309598ce5cf80326500b7d0337f0e2
s3anAK <56090826+s3anAK@users.noreply.github.com>
2023-02-15 13:31:04 -0800

    steps 1-3

--- grade.sh
+++ grade.sh
@@ -2,4 +2,15 @@ CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 git clone $1 student-submission
-echo 'Finished cloning'
+cd student-submission
+#echo 'Finished cloning'
+
+if [[ -f ListExamples.java ]]
+then
+    echo 'Correct file submitted'
+else
+    echo 'ListExamples was not found'
+    exit 1
+fi
+
+cp ../TestListExamples.java ./
