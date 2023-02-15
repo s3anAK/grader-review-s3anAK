4b67a2ca4a3b0ae9d0d78877f9a5cf903f4fec87
s3anAK <56090826+s3anAK@users.noreply.github.com>
2023-02-15 14:31:33 -0800

    added more capabilities

--- grade.sh
+++ grade.sh
@@ -3,7 +3,7 @@ CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
 rm -rf student-submission
 git clone $1 student-submission
 cd student-submission
-#echo 'Finished cloning'
+echo 'Finished cloning'
 
 if [[ -f ListExamples.java ]]
 then
@@ -19,37 +19,40 @@ javac -cp $CPATH *.java
 if [[ $? -eq 1 ]]
 then
     echo 'The tests did not compile'
+    exit 1
 else
     echo 'The tests compiled!'
-
-    javac ListExamples.java
-
-    if [[ $? -eq 1 ]]
-    then
-        echo 'ListExamples did not compile'
-    else
-        echo 'ListExamples compiled!'
-
-        java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
-
-        failures=`grep -Po 'Failures:\s\K.' output.txt`
-        OK=`grep -Po 'OK\s\(\K.' output.txt`
-        testsRun=`grep -Po 'Tests\srun:\s\K.' output.txt`
-
-        if [[ -z $failures ]]
-        then
-            echo Your grade was 100%!
-            echo You passed $OK test\(s\)!
-        else
-
-            echo There are $failures failure\(s\)
-
-            grade=$((($testsRun-$failures)/$testsRun))%
-
-            echo Your grade was $grade
-        fi
-    fi
 fi
+
+javac ListExamples.java
+
+if [[ $? -eq 1 ]]
+then
+    echo 'ListExamples did not compile'
+    exit 1
+else
+    echo 'ListExamples compiled!'
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
+
+failures=`grep -Po 'Failures:\s\K\d+' output.txt`
+OK=`grep -Po 'OK\s\(\K\d+' output.txt`
+testsRun=`grep -Po 'Tests\srun:\s\K\d+' output.txt`
+
+if [[ -z $failures ]]
+then
+    echo Your grade was 100%!
+    echo You passed all $OK test\(s\)!
+else
+
+    echo There are $failures failure\(s\)
+
+    grade=$((($testsRun-$failures)/$testsRun))%
+
+    echo Your grade was $grade
+fi
+
 
 
 
